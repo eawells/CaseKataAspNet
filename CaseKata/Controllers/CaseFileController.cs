35ffc1f086190338ced72f2210e972c5bf0dde3f
Elizabeth Wells <elizabeth.a.wells1@gmail.com>
2018-07-26 15:51:55 -0500

    When a user searches for a case that doesn't exist, we should tell them it doesn't exist

---
+++ CaseKata/Controllers/CaseFileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaseKata.Controllers
+{
+    [Route("api/casefile")]
+    [ApiController]
+    public class CaseFileController : ControllerBase
+    {
+        //GET api/casefile/30
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int docketId)
+        {
+            return "Resource Not Found";
+        }
+    }
+}
--- CaseKataTest/UnitTest1.cs
+++ CaseKataTest/UnitTest1.cs
@@ -1,13 +1,33 @@
 using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CaseKataTest
 {
     public class UnitTest1
     {
         [Test]
-        public void Test1()
+        public async Task WhenHitValues5ThenReceiveValue()
         {
+            var _client = new HttpClient();
+            
+            var response = await _client.GetAsync("http://localhost:5000/api/values/5");
+            
+            Assert.IsTrue(response.IsSuccessStatusCode);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual("value", content);
+        }
 
+        [Test]
+        public async Task GivenCaseRequestWhenCaseDoesNotExistThenReturnNotFound()
+        {
+            var _client = new HttpClient();
+            
+            var response = await _client.GetAsync("http://localhost:5000/api/casefile/30");
+            
+            Assert.IsTrue(response.IsSuccessStatusCode);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual("Resource Not Found", content);
         }
     }
 }
